53971a5878c13de6728f2a78d6b65852dd9476d0
macbookpro <kukkukhadija@gmail.com>
2024-09-16 00:52:00 +0400

    update test 1

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue= 100.0;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMilesValue = cashValue/0.0035;
+        assertEquals(expectedMilesValue,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue= 10000.0;
+        var rewardValue = new RewardValue(milesValue, true);
+        double expectedCash = milesValue*0.0035;
+        assertEquals(expectedCash,rewardValue.getCashValue());
     }
 }
